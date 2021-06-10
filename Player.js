83d8d5fd8bedb9ad1fb639f31a83e3737aacee6f
Riskayu06 <72180116+Riskayu06@users.noreply.github.com>
2021-06-10 12:15:50 +0700

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,57 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        movementSpeed: cc.Float,
+    },
+
+    onLoad() {
+        this.turnRight = false;
+        this.turnLeft = false;
+
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+    },
+
+    start () {
+
+    },
+    // DeltaTime = 1 / current FPS
+    update(dt) {
+        if (this.turnRight) {
+            this.node.x += this.movementSpeed * dt;
+        }
+        else if (this.turnLeft) {
+            this.node.x -= this.movementSpeed * dt;
+        }
+    },
+
+    onKeyDown(event) {
+        switch(event.keyCode) {
+            case cc.macro.KEY.right:
+                this.node.x += 10;  
+                break;
+            case cc.macro.KEY.left:
+                this.node.x -= 10;  
+                break;
+        }
+    },
+
+    onKeyUp(event) {
+        switch(event.keyCode) {
+            case cc.macro.KEY.right:
+                if (this.turnRight) {
+                    this.turnRight = false;
+                }   
+
+                break;
+
+            case cc.macro.KEY.left:
+                if (this.turnLeft) {
+                    this.turnLeft = false;
+                }
+
+                break;
+        }
+    },
+});
